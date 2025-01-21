2ab29c9e5a0e06ffa82a1fea21fcb9608e612ae2
namangarg31 <namangarg040@gmail.com>
2025-01-21 21:02:44 +0530

    adding test cases and buildspec file

--- quickCab/src/test/java/com/namangarg/project/quickCab/QuickCabApplicationTests.java
+++ quickCab/src/test/java/com/namangarg/project/quickCab/QuickCabApplicationTests.java
@@ -1,13 +1,8 @@
 package com.namangarg.project.quickCab;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class QuickCabApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
